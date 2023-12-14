00bc3543fe7ab2d74e7a2b770b3b96d993db575e
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2023-12-14 19:59:54 -0300

    Finalizando código API com Express.

--- node_6/projeto/src/servidor.js
+++ node_6/projeto/src/servidor.js
@@ -4,17 +4,49 @@ const porta = 3003;
 
 const express = require("express");
 // Instancia e atribui o resultado para app e encima de app começamos a colocar nossos serviços
+
+
 const app = express();
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// app.get("/produtos", (req, res, next) => {
+//   console.log("Middleware 1...");
+//   next();
+// });
+// Get é uma forma de requisição. Passamos como resposta para uma requisição encima de produtos uma função middleware
 
 app.get("/produtos", (req, res, next) => {
-  console.log("Middleware 1...");
-  next();
+  res.send(bancoDeDados.getProdutos());
 });
-// Get é uma forma de requisição. Passamos como resposta para uma requisição encima de produtos uma função middleware
-app.get("/produtos", (req, res, next) => {
-  console.log("Middleware 0...");
-  res.send({ nome: "Notebook", preco: 123.45 }); // Converte automaticamente para JSON
-});
+
+app.get('/produtos/:id', (req, res, next) => {
+  res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.nome,
+    preco: req.body.preco
+  })
+  res.send(JSON.stringify(produto)) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    id: req.params.id,
+    nome: req.body.nome,
+    preco: req.body.preco
+  })
+  res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+  const produto = bancoDeDados.excluirProduto(req.param.id)
+  res.send(produto) //JSON
+})
 
 // A porta que minha aplicação vai ficar escutando
 app.listen(porta, () => {
